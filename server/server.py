41d3216e16ab783abfb2d92d2de3839ab1a2f37c
Stephanie Dominguez <s.dominguez@javeriana.edu.co>
2020-04-29 15:10:01 -0700

    Map config

--- server/server.py
+++ server/server.py
@@ -1,5 +1,9 @@
 import os
 from flask import Flask, render_template
+from urllib.request import urlopen
+import json
+import pandas as pd
+import plotly.express as px
 app = Flask(__name__)
 
 # Import controller
@@ -12,6 +16,22 @@ def hello( ):
 
 # Bind other url
 app.add_url_rule('/decision', view_func=ctr.get_decision)
+
+@app.route("/map/")
+def map( ):
+    with urlopen('https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json') as response:
+        counties = json.load(response)
+    df = pd.read_csv("https://raw.githubusercontent.com/plotly/datasets/master/fips-unemp-16.csv",
+                   dtype={"fips": str})
+    #counties["features"][0]
+    fig = px.choropleth(df, geojson=counties, locations='fips', color='unemp',
+                           color_continuous_scale="Viridis",
+                           range_color=(0, 12),
+                           scope="usa",
+                           labels={'unemp':'unemployment rate'}
+                          )
+    fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+    fig.show()
 
 # TODO: Probably start scheduling here??
 
