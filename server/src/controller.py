650373b24c55a4e6a2d8788e649d588542d9825d
Punyisa Kraisang <ytterbium.zany@gmail.com>
2020-04-29 14:48:12 -0700

    Add request validation

--- server/src/controller.py
+++ server/src/controller.py
@@ -1,15 +1,25 @@
-from flask import request
+import flask
 import src.service as service
 
 def get_decision():
-    # Retrieve input arguments
-    args = request.args
-    location = args['location']
-    customer_count = args['count']
+    # Prepare response
+    data = {"success": False}
 
-    # TODO: Validate input
+    # Retrieve input arguments
+    args = flask.request.args
+
+    # Validate input
+    if not service.validate_args(args):
+        return flask.jsonify(data)
+        
+    location = args['location']
+    customer_count = int(args['count'])
 
     # Get decision based on the arguments
     decision = service.get_decision(location, customer_count)
 
-    return decision
+    # Create success response
+    data["decision"] = decision
+    data["success"] = True
+
+    return flask.jsonify(data)
--- server/src/service.py
+++ server/src/service.py
@@ -1,5 +1,27 @@
+import sys
+import flask
+app = flask.current_app
+
 def get_decision(location, customer_count):
     # TODO: Implement service to get estimated infected number
     #       from location, customer count, etc
 
     return "For a store in " + location + " with " + str(customer_count) + " customers ==> O]-["
+
+def validate_args(args):
+    if args == None:
+        return False
+        
+    location = args['location']
+    customer_count = args['count']
+
+    if location == None or location == "":
+        return False
+
+    if customer_count == None or customer_count == "":
+        return False
+
+    if not(isinstance(location, str)) or not(customer_count.isdigit()):
+        return False
+    
+    return True
