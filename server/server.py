ffde26d40b6744e5e19e6d69beaa46a977452159
Punyisa Kraisang <ytterbium.zany@gmail.com>
2020-04-29 17:26:02 -0700

    Move ui code to ui_controller file

--- server/server.py
+++ server/server.py
@@ -1,44 +1,28 @@
 import os
 from flask import Flask, render_template
-from urllib.request import urlopen
-import json
-import pandas as pd
-import plotly.express as px
 import schedule
 import time
-
 
 app = Flask(__name__)
 
 # Import controller
 import src.controller as ctr
+import src.ui_controller as ui
 
 # Initiate root url
 @app.route("/")
 def hello( ): 
-    return render_template("index.html", template_folder='template'); 
+    return render_template("index.html", template_folder='template') 
 
-# Bind other url
+# Bind service url
 app.add_url_rule('/decision', view_func=ctr.get_decision)
 
-@app.route("/map/")
-def map( ):
-    with urlopen('https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json') as response:
-        counties = json.load(response)
-    df = pd.read_csv("https://raw.githubusercontent.com/plotly/datasets/master/fips-unemp-16.csv",
-                   dtype={"fips": str})
-    #counties["features"][0]
-    fig = px.choropleth(df, geojson=counties, locations='fips', color='unemp',
-                           color_continuous_scale="Viridis",
-                           range_color=(0, 12),
-                           scope="usa",
-                           labels={'unemp':'unemployment rate'}
-                          )
-    fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
-    fig.show()
+# Bind ui url
+app.add_url_rule('/map', view_func=ui.map)
 
 # TODO: Is that good? 
 def scheduling():
     schedule.every().day.at("21:30").do(map)
+
 # Run application
 app.run(host='0.0.0.0', port=8018)
